9d22f14e98d95e2eb47e952f0abf29d671b4f037
Srijan Bhattacharyya. <78896721+srijan-76448@users.noreply.github.com>
2024-09-12 21:07:23 +0000

    Delete src/05-08-2024 directory

--- src/05-08-2024/Q2.sh
+++
@@ -1,9 +0,0 @@
-echo "-------------"
-echo -e "\t\tUntil Loop Example"
-echo "----------------"
-i=1
-until [ $i -gt 5 ]
-do
-   echo $i
-   i=`expr $i + 1`
-done
--- src/05-08-2024/Q3.sh
+++
@@ -1,6 +0,0 @@
-echo for in multi vers
-
-for i in 12 14 15 17 18 21
-do 
-    echo $i
-done
